745ad195e06dcc0c96b9eaf5d71090593a464e4d
tilius <tilius.sod@gmail.com>
2020-01-18 13:43:37 +0100

    master

--- index.js
+++ index.js
@@ -1,46 +1,3 @@
-document.addEventListener('DOMContentLoaded', ready);
-function ready() {
-  playVideosOnPosition();
-  returnToTopBtn();
-}
-
-function playVideosOnPosition() {
-  let videos = document.querySelectorAll('iframe');
-  let videosEmbedded = document.querySelectorAll('video');
-  let options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0
-  };
-  let observer = new IntersectionObserver(turnVideosOn, options);
-
-  videosEmbedded.forEach(video => {
-    observer.observe(video);
-  });
-}
-
-let turnVideosOn = (entries, observer) => {
-  entries.forEach(entry => {
-    let video = entry.target;
-
-    if (entry.isIntersecting) {
-      console.log('play');
-      video.play();
-      // if (video.src.includes('?')) {
-      //   //TODO: add an alert or notification to enable Flash?! for Chrome to work,
-      //   // Safari also needs to allow Autoplay.
-      //   video.src += '&autoplay=1';
-      // } else {
-      //   video.src += '?&autoplay=1';
-      // }
-    } else {
-      console.log('stop');
-      video.pause();
-      // video.src = video.src.replace('&autoplay=1', '');
-    }
-  });
-};
-
 function returnToTopBtn() {
   let btn = document.querySelector('#return-btn');
   let lastScrollTop = pageYOffset || document.scrollTop;
@@ -50,11 +7,11 @@ function returnToTopBtn() {
     function() {
       let st = window.pageYOffset || document.scrollTop;
       if (st > lastScrollTop) {
-        btn.style.opacity = '0';
+        btn.style.opacity = '1';
       } else {
         btn.style.opacity = '1';
         this.setTimeout(() => {
-          btn.style.opacity = '0';
+          btn.style.opacity = '1';
         }, 5000);
       }
       lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
